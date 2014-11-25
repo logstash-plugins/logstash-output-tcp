f3ea758787a391309c132d4cbb095c642e4a8888
Joao Duarte <jsvduarte@gmail.com>
2014-11-25 17:43:30 +0000

    add event to on_event callback

--- lib/logstash/outputs/tcp.rb
+++ lib/logstash/outputs/tcp.rb
@@ -87,7 +87,7 @@ class LogStash::Outputs::Tcp < LogStash::Outputs::Base
         end
       end
 
-      @codec.on_event do |payload|
+      @codec.on_event do |event, payload|
         @client_threads.each do |client_thread|
           client_thread[:client].write(payload)
         end
@@ -95,7 +95,7 @@ class LogStash::Outputs::Tcp < LogStash::Outputs::Base
       end
     else
       client_socket = nil
-      @codec.on_event do |payload|
+      @codec.on_event do |event, payload|
         begin
           client_socket = connect unless client_socket
           r,w,e = IO.select([client_socket], [client_socket], [client_socket], nil)
